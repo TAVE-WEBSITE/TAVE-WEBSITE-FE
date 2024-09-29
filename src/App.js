5b51eda309cf1b578071ebc463df67c6e9dfa03a
Yeong-si <dtd1110@gmail.com>
2024-09-29 21:18:12 +0900

    FEAT : 홈페이지 헤더 #1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import logo from './logo.svg';
-import './index.css';
-import './App.css';
+import logo from "./logo.svg";
+import "./index.css";
+import "./App.css";
+import "./styles/global.css";
+import { Header } from "./components/Header";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    // 임시 테스트용 코드
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
+/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>*/
 export default App;
