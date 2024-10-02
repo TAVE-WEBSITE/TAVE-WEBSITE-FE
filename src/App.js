37511f1adf885b1737f70a09251cd96dbf5901b4
tldms0507 <sieun050783@gmail.com>
2024-10-02 16:42:27 +0900

    FIX : 컴포넌트 호출 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
-import './index.css';
-import './App.css';
+// import logo from "./logo.svg";
+import "./index.css";
+import "./App.css";
 import "./styles/global.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Header } from "./components/header";
-import Home from "./pages/home";
+import { Header } from "./components/Header";
+import File from "./components/file";
 
 function App() {
-    return (
- // 임시 테스트용 코드
- <div className="App">
- <Header></Header>
-</div>
-    );
+  return (
+    // 임시 테스트용 코드
+    <div className="App">
+      <Header></Header>
+      {/* study File */}
+      <File
+        type="study"
+        title="스터디 이름 캬캬 아기하마"
+        teamNum={14}
+        teamName="아기하마"
+      />
+      {/* project File */}
+      <File
+        type="project"
+        title="플젝 이름 캬캬 아기하마"
+        teamNum={14}
+        teamName="아기하마"
+      />
+    </div>
+  );
 }
 
 export default App;
