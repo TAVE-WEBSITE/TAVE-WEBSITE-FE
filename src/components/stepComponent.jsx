7ba741c9c19f08d4ed37efc11d172af4e23c3624
tldms0507 <sieun050783@gmail.com>
2024-10-19 15:13:00 +0900

    DESIGN : 헤더 색상 변경

--- src/components/stepComponent.jsx
+++ src/components/stepComponent.jsx
@@ -5,38 +5,45 @@ export default function StepComponent({ steps, selectedStep }) {
   return (
     <div className="flex items-start">
       {/* 정규 세션 단계 */}
-      <div className="flex flex-col gap-5  ">
+      <div className="flex flex-col gap-5">
         {steps.map((step, index) => (
           <div key={index} className="flex items-center gap-5 relative">
             {/* 단계별 Dot */}
             <img
               src={selectedStep === index ? ActiveDot : NotActiveDot}
-              className={`z-10 transition-all duration-300 ease-in-out`}
+              className="z-10 transition-all duration-300 ease-in-out"
               alt="StepDot"
             />
-            
+
             {/* 단계별 수직 라인 */}
-            {/* 첫번째와 마지막 단계는 라인이 Dot 밖으로 나오지 않게 길이와 top 조정 */}
-            {index !== steps.length  && (
+            {index !== steps.length && (
               <div
-                className={`absolute left-3 ${index === 0? "bottom-0 h-[90px]" : "top-50% h-[200px]"}  ${index === 8? "top-0 h-[90px]" : "top-50% h-[200px]"} w-[3px]  transition-all duration-500 ease-in-out ${
+                className={`absolute left-3 ${
+                  index === 0 ? "bottom-0 h-[80px]" : "top-50% h-[200px]"
+                } ${
+                  index === 8 ? "top-0 h-[80px]" : "top-50% h-[200px]"
+                } w-[3px] transition-all duration-500 ease-in-out ${
                   selectedStep === index
                     ? "bg-gradient-to-b from-[rgb(36,36,36)] via-[rgb(39,76,200)] to-[rgb(36,36,36)]"
                     : "bg-[rgb(36,36,36)]"
                 }`}
               />
             )}
-            
-            {/* 정규 세션 타이틀, 내용 소개 div */}
+
+            {/* 정규 세션 타이틀, 내용 소개 + selectedStep일 때 크기 조금 커지게 */}
             <div
-              className={`flex justify-between gap-3 items-center w-92 lg:min-w-[550px] h-36 rounded-2xl p-5 transition-all duration-300 ease-in-out ${
+              className={`flex justify-between gap-3 items-center w-92 lg:min-w-[550px] h-36 rounded-2xl p-5 transition-transform duration-300 ease-in-out ${
+                selectedStep === index ? "scale-105" : "scale-100"
+              } transition-all duration-300 ease-in-out ${
                 selectedStep === index
                   ? "bg-[rgb(39,76,200)] text-white"
                   : "bg-[rgba(36,36,36,0.7)] text-gray-500"
-              } lg:w-[800px] `}
+              } lg:w-[800px]`}
             >
               <div className="text-xl lg:text-2xl">{step.title}</div>
-              <div className="text-base font-medium lg:text-lg">{step.description}</div>
+              <div className="text-base font-medium lg:text-lg">
+                {step.description}
+              </div>
             </div>
           </div>
         ))}
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -173,11 +173,11 @@ export default function Project() {
 
   return (
     <div className="grid place-items-center">
-      <div className="py-[88px] grid place-items-center">
-        <div className="text-4xl font-bold leading-[58px] pb-[16px]">
+      <div className="mt-20 py-24 grid place-items-center w-4/5 break-keep text-center">
+        <div className="text-4xl font-bold leading-14 pb-4 ">
           TAVE의 프로젝트를 소개합니다
         </div>
-        <div className="text-base font-medium leading-[20px]">
+        <div className="text-base font-medium leading-14">
           자세한 내용이 궁금하다면? 아래 링크를 통해 프로젝트 후기를 볼 수
           있습니다!
         </div>
@@ -185,7 +185,7 @@ export default function Project() {
       {/*Tab 컴포넌트 부분 */}
       <div className="product w-full max-w-6xl">
         <div
-          className={`tabs-container relative ${
+          className={`tabs-container relative px-5  break-keep text-center ${
             isTabFixed
               ? "sticky top-20 z-20 bg-gradient-to-b from-black from-40% to-transparent w-full max-w-6xl"
               : ""
@@ -205,7 +205,7 @@ export default function Project() {
         {/* 프로젝트 내용 부분 */}
         <div className="content pb-96">
           <div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6 mt-[48px]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6 mt-12 justify-items-center">
               {filteredFileSet.map((data, index) => (
                 <File
                   key={index}
