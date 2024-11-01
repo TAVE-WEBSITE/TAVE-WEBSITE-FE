2db625aa503674e37be5b50f17313f21ab06c87d
Yeong-si <dtd1110@gmail.com>
2024-11-02 00:12:33 +0900

    FEAT : 홈페이지 내용 구현 #19

--- src/core/router.jsx
+++ src/core/router.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from '../components/header';
-import Home from '../pages/home';
-import Apply from '../pages/apply';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/header";
+import Home from "../pages/home";
+import Apply from "../pages/apply";
 
-import Study from '../pages/study';
-import Project from '../pages/project';
+import Study from "../pages/study";
+import Project from "../pages/project";
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/study" element={<Study />} />
-                <Route path="/project" element={<Project />} />
-                <Route path="/apply" element={<Apply />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/study" element={<Study />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/apply" element={<Apply />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,40 @@
-import File from "../components/file";
-import '../styles/global.css';
-const Home = () => {
+import "../styles/global.css";
+import History from "../components/history";
+import Footer from "../components/footer";
+import Sponsored from "../components/sponsored";
+import Review from "../components/review";
+
+export default function Home() {
   return (
-    <div>
-      {/* 스터디 props를 호출 */}
-      <File 
-        type="study" 
-        title="스터디 이름 캬캬 아기하마" 
-        teamNum={14} 
-        teamName="아기하마" 
-      />
-      {/* project props를 호출 */}
-      <File 
-        type="project" 
-        title="플젝 이름 캬캬 아기하마" 
-        teamNum={14} 
-        teamName="아기하마" 
-      />
+    <div className="flex flex-col justify-center items-center mx-20">
+      <div className="flex flex-col py-[88px] justify-center items-center">
+        <div className="text-4xl font-bold leading-[58px] pb-[16px]">
+          TAVE HISTORY
+        </div>
+        <div className="font-medium leading-[20px] mb-20">
+          TAVE 프로젝트의 활동 내역 및 수상 내역을 확인해보세요
+        </div>
+        <History></History>
+      </div>
+      <div className="flex flex-col py-[88px] justify-center items-center">
+        <div className="text-4xl font-bold leading-[58px] pb-[16px]">
+          TAVE Review
+        </div>
+        <div className="font-medium leading-[20px] mb-20  text-center">
+          TAVE와 함께한 사람들의 후기를 들어보세요.
+          <Review></Review>
+        </div>
+      </div>
+      <div className="flex flex-col py-[88px] justify-center items-center">
+        <div className="text-4xl font-bold leading-[58px] pb-[16px]">
+          Sponsored by
+        </div>
+        <div className="font-medium leading-[20px] mb-20 text-center">
+          <div>TAVE와 함께하는 공식 파트너 단체입니다.</div>
+          <Sponsored />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-};
-
-export default Home;
+}
