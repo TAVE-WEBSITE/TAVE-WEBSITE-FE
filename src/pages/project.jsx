62ee60800e0828f64a86b5cf666d50fe9bf41912
PangKiYeon <pdrkeamy@gmail.com>
2024-11-07 00:27:11 +0900

    fix : 안쓰는 훅 삭제

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Tab from '../components/tab';
 import File from '../components/file';
 
