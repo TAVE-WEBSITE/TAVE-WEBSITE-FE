5986b08d0bb3cdd33f2af5a9951172cfedcd540a
PangKiYeon <pdrkeamy@gmail.com>
2024-11-06 10:42:31 +0900

    fix: App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 // import logo from "./logo.svg";
-import "./index.css";
-import "./App.css";
-import "./styles/global.css";
-
-import Header from "./components/header";
-import File from "./components/file";
+import './index.css';
+import './App.css';
+import './styles/global.css';
 
 function App() {
-  return (
-    // 임시 테스트용 코드
-    <div className="App">
-      <Header></Header>
-      {/* study File */}
-      <File
-        type="study"
-        title="스터디 이름 캬캬 아기하마"
-        teamNum={14}
-        teamName="아기하마"
-      />
-      {/* project File */}
-      <File
-        type="project"
-        title="플젝 이름 캬캬 아기하마"
-        teamNum={14}
-        teamName="아기하마"
-      />
-    </div>
-  );
+    return <div className="App"></div>;
 }
 
 export default App;
