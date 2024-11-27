47f09f161e959f0c64154f66125530923bc8bb1f
tldms0507 <sieun050783@gmail.com>
2024-11-27 14:59:27 +0900

    design : hover css 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LogoTave from '../assets/images/LogoTave.svg';
+import HeaderHover from '../assets/images/HeaderHover.svg';
 
 export default function Header({ isBlack }) {
     return (
@@ -11,29 +12,22 @@ export default function Header({ isBlack }) {
             }`}
         >
             <a href="/" className="items-start">
-                <img
-                    src={LogoTave}
-                    className="w-32 h-auto"
-                    alt="taveLogo"
-                ></img>
+                <img src={LogoTave} className="w-32 h-auto" alt="taveLogo" />
             </a>
             <nav className="flex items-end text-white">
                 <ul className="flex items-center gap-x-10">
-                    <li>
-                        <a href="activity" className="cursor-pointer">
-                            ACTIVITY
-                        </a>
-                    </li>
-                    <li>
-                        <a href="study" className="cursor-pointer">
-                            STUDY
-                        </a>
-                    </li>
-                    <li>
-                        <a href="project" className="cursor-pointer">
-                            PROJECT
-                        </a>
-                    </li>
+                    {['ACTIVITY', 'STUDY', 'PROJECT'].map((item, index) => (
+                        <li key={index} className="relative group">
+                            <a href={item.toLowerCase()} className="cursor-pointer relative z-10">
+                                {item}
+                            </a>
+                            <img
+                                src={HeaderHover}
+                                alt=""
+                                className="absolute top-2 right-[-10px] transform -translate-y-1/2 ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                            />
+                        </li>
+                    ))}
                     <li>
                         <div className="bg-gradient-to-r from-[#6D3CFF] to-[#1A5BFF] py-2 px-6 rounded-2xl text-center">
                             <a href="apply" className="cursor-pointer">
