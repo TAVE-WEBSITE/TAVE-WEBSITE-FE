4c4e11e1c63a2f7a3362b1e8f8fe42f5bbf7aaa5
PangKiYeon <pdrkeamy@gmail.com>
2025-01-07 21:34:19 +0900

    fix : 스크롤 애니메이션 맞게 수정

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,15 +21,13 @@ export default function Home() {
                         기술의 물결 속에서 함께 성장하는 곳, TAVE
                     </div>
                 </div>
-                <div className="h-screen flex justify-center items-center">
-                    <p className="text-4xl font-bold text-center leading-[58px]">
+
+                <div className="h-screen w-full flex flex-col items-center justify-center ">
+                    <p className="text-4xl font-bold text-center leading-[58px] mb-20">
                         다양한 분야와의 융합을 통해 미래를 선도하는
                         <br />
                         IT 연합 동아리, TAVE를 소개합니다
                     </p>
-                </div>
-
-                <div className="h-screen w-full flex flex-col items-center justify-center px-20">
                     <div className="flex flex-col items-center gap-4">
                         <div className="text-4xl font-bold">Core Value</div>
                         <div className="text-xl font-light">TAVE 15기의 핵심가치를 소개합니다</div>
