51a6121f49e12b2a326360ca49f92db5bd1bb70d
tldms0507 <sieun050783@gmail.com>
2025-02-08 16:34:08 +0900

    design : File Componenet 이미지 변경

--- src/components/file.jsx
+++ src/components/file.jsx
@@ -1,61 +1,28 @@
 import React, { useMemo } from "react";
-import ProjectBackImage from "../assets/images/ProjectBack.png";
-import StudyFront1 from "../assets/images/StudyFront1.png";
-import StudyFront2 from "../assets/images/StudyFront2.png";
-import StudyFront3 from "../assets/images/StudyFront3.png";
-import Back1 from "../assets/images/Back1.png";
-import Back2 from "../assets/images/Back2.png";
-import Back3 from "../assets/images/Back3.png";
+import StudyFile1 from "../assets/images/StudyFile1.svg";
+import StudyFile2 from "../assets/images/StudyFile2.svg";
+import StudyFile3 from "../assets/images/StudyFile3.svg";
+import ProjectFile1 from "../assets/images/ProjectFile1.svg";
+import ProjectFile2 from "../assets/images/ProjectFile2.svg";
+import ProjectFile3 from "../assets/images/ProjectFile3.svg";
 
-const studyVariants = [
-  { front: StudyFront1, back: Back1 },
-  { front: StudyFront2, back: Back2 },
-  { front: StudyFront3, back: Back3 },
-];
+const studyVariants = [StudyFile1, StudyFile2, StudyFile3];
+const projectVariants = [ProjectFile1, ProjectFile2, ProjectFile3];
 
-const projectVariants = [
-  { front: StudyFront1, back: Back1 },
-  { front: StudyFront2, back: Back2 },
-  { front: StudyFront3, back: Back3 },
-]
 const File = ({ type, title, field, teamNum, teamName, description, blogUrl, topic }) => {
-
-
-  const studyVariant = useMemo(() => {
-    return studyVariants[Math.floor(Math.random() * studyVariants.length)];
-  }, []);
-
-  const projectVariant = useMemo(() => {
-    return projectVariants[Math.floor(Math.random() * studyVariants.length)];
-  }, []);
-
-  const backgroundImage = type === "study" ? studyVariant.back : ProjectBackImage;
-
-  const backgroundStyle = type === "study"
-  ? {
-      backgroundImage: `url(${studyVariant.back})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center"
+  const backgroundImage = useMemo(() => {
+    if (type === "study") {
+      return studyVariants[Math.floor(Math.random() * studyVariants.length)];
+    } else {
+      return projectVariants[Math.floor(Math.random() * projectVariants.length)];
     }
-  : {
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center"
-    };
+  }, [type]);
 
-const frontStyle = type === "study"
-  ? {
-      backgroundImage: `url(${studyVariant.front})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-    }
-  : {
-    backgroundImage: `url(${projectVariant.front})`,
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
     backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
-    };
+  };
 
   const handleClick = () => {
     if (blogUrl) {
@@ -68,18 +35,17 @@ const frontStyle = type === "study"
   return (
     <div>
       <div
-        className="relative w-[194px] h-[164px] md:w-[269px] md:h-[230px] bg-no-repeat cursor-pointer"
+        className="relative w-48 h-[10.5rem] md:w-64 md:h-56 bg-no-repeat cursor-pointer"
         style={backgroundStyle}
         onClick={handleClick}
       >
-        <div
-    className="absolute w-[194px] h-[140px] md:w-[270px] md:h-[195px] mt-7 md:mt-[44px] flex items-center bg-no-repeat justify-center "
-    style={frontStyle}
-  >
-        </div>
-        <div className="absolute bottom-0 left-0 p-4 text-white">
-          <h2 className="text-sm md:text-lg font-bold font-Pretendard"> {type === "study" ? topic : description}</h2>
-          <p className="text-xs font-normal font-Pretendard">{teamNum}기  {type === "study" ? teamName : title}팀</p>
+        <div className="absolute bottom-0 left-0 px-4 py-8 text-white">
+          <h2 className="text-sm md:text-lg font-bold font-Pretendard break-keep">
+            {type === "study" ? topic : description}
+          </h2>
+          <p className="text-xs font-normal font-Pretendard">
+            {teamNum}기 {type === "study" ? teamName : title}팀
+          </p>
         </div>
       </div>
     </div>
