c5b85a0456fc6a4d8700529a18a69e1c97b35106
Yeong-si <dtd1110@gmail.com>
2025-02-10 21:30:56 +0900

    design : 스폰서 크기 수정

--- src/components/sponsored.jsx
+++ src/components/sponsored.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { ReactComponent as CodeitLogo } from '../assets/images/CodeitLogo.svg';
-import { ReactComponent as AlpacoLogo } from '../assets/images/AlpacoLogo.svg';
-import { ReactComponent as BBCareerLogo } from '../assets/images/BBCareerLogo.svg';
-import {ReactComponent as LetsCareerLogo} from "../assets/images/home/LetsCareerLogo.svg";
+import React from "react";
+import { ReactComponent as CodeitLogo } from "../assets/images/CodeitLogo.svg";
+import { ReactComponent as AlpacoLogo } from "../assets/images/AlpacoLogo.svg";
+import { ReactComponent as BBCareerLogo } from "../assets/images/BBCareerLogo.svg";
+import { ReactComponent as LetsCareerLogo } from "../assets/images/home/LetsCareerLogo.svg";
 
 //고화질로 변경
 
 export default function Sponsored() {
-    return (
-        <>
-        <div className="grid grid-cols-2 md:grid-cols-4 justify-items-center gap-4 md:gap-6">            
-            <CodeitLogo className='w-32 h-32 md:w-48 md:h-48'/>
-            <AlpacoLogo className='w-32 h-32 md:w-48 md:h-48' />
-            <BBCareerLogo className='w-32 h-32 md:w-48 md:h-48' />
-           <LetsCareerLogo className='w-32 h-32 md:w-48 md:h-48'/>
-        </div>
-        
-        </>
-    );
+  return (
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-4 justify-items-center gap-4 md:gap-6">
+        <CodeitLogo className="w-32 h-32 lg:w-48 lg:h-48" />
+        <AlpacoLogo className="w-32 h-32 lg:w-48 lg:h-48" />
+        <BBCareerLogo className="w-32 h-32 lg:w-48 lg:h-48" />
+        <LetsCareerLogo className="w-32 h-32 lg:w-48 lg:h-48" />
+      </div>
+    </>
+  );
 }
